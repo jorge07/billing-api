dcd9d3bd911bb700436e33c30a3c98e52b977a83
Jorge Arco <jorge.arcoma@gmail.com>
2019-11-08 15:32:30 +0100

    Fix error handling

--- src/application/transaction/create/handler.ts
+++ src/application/transaction/create/handler.ts
@@ -2,13 +2,13 @@ import { Application } from "hollywood-js";
 import { inject, injectable } from "inversify";
 import IRepository from "../../../domain/transaction/repository";
 import Transaction from "../../../domain/transaction/transaction";
-import Log from "../../../infrastructure/shared/audit/logger";
+import { ILog } from "../../../infrastructure/shared/audit/logger";
 import CreateCommand from "./command";
 
 @injectable()
 export default class Create implements Application.ICommandHandler {
     constructor(
-        @inject("logger") private readonly logger: Log,
+        @inject("logger") private readonly logger: ILog,
         @inject("domain.transaction.repository") private readonly repository: IRepository,
     ) {}
 
--- src/ui/http/routing/transaction/post.ts
+++ src/ui/http/routing/transaction/post.ts
@@ -5,7 +5,6 @@ import { IRoute } from "../index";
 
 export default function create(app: App): IRoute {
     return {
-
         action: async (req: Request, res: Response) => {
             const { uuid, product, price } = req.body;
 
