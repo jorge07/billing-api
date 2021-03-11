22c58c24b8990c6b9e6152d59002c5ec775cc40b
Jorge Arco <jorge.arcoma@gmail.com>
2021-03-11 11:21:52 +0100

    Update src/Kernel.ts
    Co-authored-by: Joshua Barnett <joshua@barne.tt>

--- src/Kernel.ts
+++ src/Kernel.ts
@@ -4,7 +4,7 @@ import { testParameters } from "../config/paramaters.test";
 import { parameters } from "../config/parameters";
 
 export default async function KernelFactory(debug: boolean): Promise<Framework.Kernel> {
-     return await Framework.Kernel.createFromModuleContext(
+     return Framework.Kernel.createFromModuleContext(
         process.env.NODE_ENV,
         debug,
         parameters,
